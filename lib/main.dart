51a973b4a2f6e6063f194acd1261ba933b59767d
Anujit-Datta <anujitbestbd@gmail.com>
2024-01-11 21:22:56 +0600

    save

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: HomePage(),
     );
@@ -28,7 +29,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "My Bag",
+              style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.8,
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    leading: Image.asset(
+                      "asset/pullover.png",
+
+                    ),
+                    title: const Text("pullover"),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
